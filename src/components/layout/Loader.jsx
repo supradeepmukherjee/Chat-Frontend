da0e9d8550da31d0d479527ebdca735ec8a04623
Supradeep <supradeep2004@gmail.com>
2024-05-25 12:38:45 +0530

    Render Loading

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -1,4 +1,4 @@
-import { Grid, Skeleton, Stack } from "@mui/material"
+import { Grid, Skeleton, Stack, Typography } from "@mui/material"
 import { BouncingSkeleton } from "../Styled"
 
 const Loader = () => (
@@ -17,7 +17,11 @@ const Loader = () => (
         </Grid>
         <Grid item xs={12} sm={8} md={5} lg={6}>
             <Stack spacing={2.4}>
-                {Array.from({ length: 12 }).map((_, i) => <Skeleton height={'4.2rem'} key={i} />)}
+                {Array.from({ length: 3 }).map((_, i) => <Skeleton height={'4.2rem'} key={i} />)}
+                <Typography className="!ml-2">
+                    Please wait. The site is deployed using a free instance on render.com which takes 50 seconds to spin up
+                </Typography>
+                {Array.from({ length: 4 }).map((_, i) => <Skeleton height={'4.2rem'} key={i} />)}
             </Stack>
         </Grid>
         <Grid
